42e46f2c6caf8cfebbd457a0d14755347b093123
mrshanto029 <sdks6409@gmail.com>
2026-09-05 22:06:34 +0600

    refactor: update route definitions and improve structure

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-
+from src.routes.productRoute import productRoutes
 app = FastAPI(
     title="My FastAPI Course",
     description="A simple FastAPI course",
     version="0.1.0"
 )
 
-@app.get("/")
+@app.get("/home")
 def home():
     return {"message": "Welcome to my FastAPI course!"}
 
 
 ####CRUD APIS -Products - JSON File
+
+app.add_routes("/", productRoutes)
--- src/routes/productRoute.py
+++ src/routes/productRoute.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 productRoutes = APIRouter()
 
-
+@productRoutes.get("/")
 def getAllProducts():
     return {"message": "Get all products"}
