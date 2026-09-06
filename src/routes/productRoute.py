70ef335aeb1aaa8f76b82e5acbf8f910d727713f
mrshanto029 <sdks6409@gmail.com>
2026-09-07 00:45:52 +0600

    feat: enhance product retrieval with detailed error handling

--- src/routes/productRoute.py
+++ src/routes/productRoute.py
@@ -1,19 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from src.utils.utils import get_all_products
 productRoutes = APIRouter()
-
-@productRoutes.get("/")
-def getAllProducts():
-    return get_all_products() 
-
+##get all products
+# @productRoutes.get("/")
+# def getAllProducts():
+#     return get_all_products() 
+##get one product by id path parameter
 @productRoutes.get("/{id}")
 def getOneProduct(id: int):
-    print("ID:", id)
     allProducts = get_all_products()
     for product in allProducts:
         if product["id"] == id:
             return product
-    return {"message": "Product not found"}
+    return HTTPException(status_code=404, detail="Product not found")
+
+##get one product by id query parameter
+@productRoutes.get("/")
+def getOneProductQuery(product_id:int = None):
+    allProducts = get_all_products()
+
+    if not product_id:
+        return allProducts
+    
+    for product in allProducts:
+        if product["id"] == product_id:
+            return product
+    return HTTPException(status_code=404, detail="Product not found")
+
+
+##create new product
 @productRoutes.post("/create")
 def createNewProduct():
     return {"message": "Create new product"}
